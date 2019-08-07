96d2400189e8a143ff3185b0bdc3a671cd56c379
Nafis Ahmad <happy56@gmail.com>
2019-08-07 09:41:13 +0600

    bug fix

--- c.py
+++ c.py
@@ -1,5 +1,17 @@
 """
+	How to use this Caching :
+	for example you need to cache this function -> duc([agumnet])
+	instead of call the function rex = duc([agumnet])
+	you change the call into rex = ncache(duc, [agumnet])
+	just One change when you call the function. 
+
+	How it is done:
 	Simple hash table for each function results for a certain time
+	
+	What can be done in future:
+	1. Get muiltple agrument from the function
+	2. Use backend Database not overloading the Ram
+
 
 """
 import time, math
